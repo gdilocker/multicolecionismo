824e9a5158e861ef4fb621a276572a523642783a
gdilocker <72871554+gdilocker@users.noreply.github.com>
2025-11-12 18:27:35 -0300

    Updated index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
       spacing: {
         '70': '280px',
       },
+      colors: {
+        'brand': {
+          gold: '#FFD700',
+          'gold-light': '#FFE44D',
+          'gold-dark': '#D4AF37',
+          black: '#1a1a1a',
+          'gray-dark': '#2d2d2d',
+          'gray': '#4a4a4a',
+          'gray-light': '#6b6b6b',
+        },
+      },
     },
   },
   plugins: [],
